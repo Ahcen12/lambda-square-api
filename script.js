b0e58ac9e031e051b760cb48bb83dd4888cd1d58
Ahcen12 <114553246+Ahcen12@users.noreply.github.com>
2025-10-22 22:37:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 async function calculateSquare() {
-  const name = document.getElementById("name").value || "Ziyaretçi";
-  const number = document.getElementById("number").value;
+  const name = document.getElementById("nameInput").value || "Ziyaretçi";
+  const number = document.getElementById("numberInput").value;
   const resultDiv = document.getElementById("result");
 
   resultDiv.innerText = "Hesaplanıyor...";
 
-
-  const apiUrl = "https://mktq4gz54j.execute-api.eu-central-1.amazonaws.com/test/squareFunction" ;
+  const apiUrl = "https://mktq4gz54j.execute-api.eu-central-1.amazonaws.com/test/squareFunction";
 
   try {
     const response = await fetch(`${apiUrl}?name=${name}&number=${number}`);
     const data = await response.json();
-    resultDiv.innerText = data.message;
+    resultDiv.innerText = data.message || "Bir çıktı dönmedi 😅";
   } catch (error) {
-    resultDiv.innerText = "Hata oluştu: " + error;
+    resultDiv.innerText = "Hata oluştu: " + error.message;
   }
 }
